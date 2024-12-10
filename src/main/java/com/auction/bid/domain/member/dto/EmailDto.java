65b547b711a249f134890413af976dd1ef0a84e7
0xc0debabe <kongminoo@naver.com>
2024-12-10 19:09:29 +0900

    refactor : 빌더 패턴 및 JSON 매핑을 위한 기본 생성자와 AllArgs생성자 추가

--- src/main/java/com/auction/bid/domain/member/dto/EmailDto.java
+++ src/main/java/com/auction/bid/domain/member/dto/EmailDto.java
@@ -2,9 +2,12 @@ package com.auction.bid.domain.member.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class EmailDto {
 
     @Email(message = "유효한 이메일을 입력하세요.")
