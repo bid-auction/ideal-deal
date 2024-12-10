ff85b4d7917d60b944012444ff89729d455cc615
0xc0debabe <kongminoo@naver.com>
2024-12-10 19:10:58 +0900

    test : 단위 테스트 및 통합 테스트 진행

---
+++ src/test/java/com/auction/bid/domain/member/MemberControllerIntegrTest.java
@@ -0,0 +1,130 @@
+package com.auction.bid.domain.member;
+
+import com.auction.bid.domain.member.dto.EmailDto;
+import com.auction.bid.domain.member.dto.SignUpDto;
+import com.auction.bid.global.security.ConstSecurity;
+import com.auction.bid.global.security.jwt.JWTUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class MemberControllerIntegrTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    SignUpDto.Request signUpReq;
+    SignUpDto.Response signUpRes;
+
+    @BeforeEach()
+    private void setUp() {
+        signUpReq = SignUpDto.Request.builder()
+                .loginId("testLoginId")
+                .password("1234567890")
+                .email("test@naver.com")
+                .nickname("testNickname")
+                .name("testName")
+                .phoneNumber("010-1234-5678")
+                .emailVerified(true)
+                .address(
+                        Address.builder()
+                                .city("seoul")
+                                .street("saemalo")
+                                .zipcode("548")
+                                .build())
+                .build();
+
+        signUpRes = SignUpDto.Response.builder()
+                .id(1L)
+                .loginId("resLoginId")
+                .name("resName")
+                .nickname("resNickName")
+                .build();
+
+    }
+
+    @Test
+    void member_signup_success() throws Exception {
+        postPerform("/auth/signup", signUpReq)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.nickname").value("testNickname"))
+                .andExpect(jsonPath("$.loginId").value("testLoginId"))
+                .andExpect(jsonPath("$.name").value("testName"));
+    }
+
+    @Test
+    @DisplayName("이메일 보내기 성공")
+    void server_send_email_success() throws Exception {
+        EmailDto emailDto = EmailDto.builder()
+                .email("kongminoo@naver.com")
+                .build();
+
+        postPerform("/auth/send-email", emailDto)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("kongminoo@naver.com"));
+    }
+
+    @Test
+    @DisplayName("ROLE 멤버 로그아웃 성공")
+    void only_role_member_logged_out() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        String accessToken = jwtUtil.generateAccessToken(
+                uuid,
+                ConstSecurity.ROLE_MEMBER,
+                5000L
+        );
+
+        mockMvc.perform(post("/api/member/auth/logout")
+                        .header(ConstSecurity.AUTHORIZATION, ConstSecurity.BEARER + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(uuid.toString()));
+    }
+
+    @Test
+    @DisplayName("ROLE 어드민 로그아웃 실패")
+    void admin_role_denied() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        String accessToken = jwtUtil.generateAccessToken(
+                uuid,
+                ConstSecurity.ROLE_ADMIN,
+                5000L
+        );
+
+        mockMvc.perform(post("/api/member/auth/logout")
+                        .header(ConstSecurity.AUTHORIZATION, ConstSecurity.BEARER + accessToken))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    private ResultActions postPerform(String uri, Object requestDto) throws Exception {
+        return mockMvc.perform(post("/api/member" + uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andDo(print());
+    }
+
+}
