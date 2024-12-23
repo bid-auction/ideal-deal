dd34a57b88a23fa2bf0e050fb4977d6e7ef85b2b
0xc0debabe <kongminoo@naver.com>
2024-12-23 22:51:43 +0900

    feat : 웹소켓 추가 구현

--- src/main/java/com/auction/bid/global/websocket/WebSocketBidHandler.java
+++ src/main/java/com/auction/bid/global/websocket/WebSocketBidHandler.java
@@ -2,11 +2,13 @@ package com.auction.bid.global.websocket;
 
 import com.auction.bid.domain.bid.BidDto;
 import com.auction.bid.domain.member.Member;
+import com.auction.bid.domain.member.MemberService;
 import com.auction.bid.domain.product.Product;
 import com.auction.bid.domain.product.ProductBidPhase;
-import com.auction.bid.domain.product.ProductService;
+import com.auction.bid.domain.product.ProductRepository;
 import com.auction.bid.global.exception.ErrorCode;
-import com.auction.bid.global.exception.exceptions.BidException;
+import com.auction.bid.global.exception.exceptions.MoneyException;
+import com.auction.bid.global.exception.exceptions.ProductException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +23,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Stream;
 
 import static com.auction.bid.global.scheduler.ConstAuction.AUCTION;
 import static com.auction.bid.global.websocket.ConstWebsocket.MEMBER;
@@ -32,8 +35,9 @@ import static com.auction.bid.global.websocket.ConstWebsocket.PRODUCT_ID;
 public class WebSocketBidHandler extends TextWebSocketHandler {
 
     private final ObjectMapper objectMapper;
-    private final ProductService productService;
+    private final ProductRepository productRepository;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final MemberService memberService;
 
     private final Set<WebSocketSession> sessions = new HashSet<>();
     private final Map<Long, Set<WebSocketSession>> roomSessionMap = new HashMap<>();
@@ -44,56 +48,60 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
         Member findMember = (Member) attributes.get(MEMBER);
         Long productId = (Long) attributes.get(PRODUCT_ID);
 
-        if (findMember == null || productId == null) {
-            closeOneSession(session);
-        }
-
         sendViewerCountToBidRoom(getSocketSetWhenEntered(productId, session));
         sessions.add(session);
 
-        List<BidDto> bidDtoList;
-        try {
-            bidDtoList = BidDto.convertToBidDtoList(
-                    bidListFromRedis(
-                            productId,
-                            roomSessionMap.get(productId)
-                    ));
-        } catch (BidException e) {
-            log.info("경매 종료={}", e.getMessage());
-            return;
-        }
+        List<BidDto> bidDtoList = BidDto.convertToBidDtoList(
+                bidListFromRedis(productId)
+        );
 
         BidDto bidDto = bidDtoList.isEmpty() ? BidDto.emptyDtoList(productId) :
                 bidDtoList.get(bidDtoList.size() - 1);
 
         sendMessageToEntrant(bidDto, session);
 
-        log.info("[{}] 연결됨", findMember.getId());
+        if (findMember == null) {
+            log.info("[sessionId={}] 연결됨", session.getId());
+        } else {
+            log.info("[memberId={}] 연결됨", findMember.getId());
+        }
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         Map<String, Object> attributes = session.getAttributes();
         Member member = (Member) attributes.get(MEMBER);
+
+        if (member == null) {
+            sendMessage(session, "로그인해야 입찰이 가능합니다.");
+            return;
+        }
+
         Long productId = (Long) attributes.get(PRODUCT_ID);
+        Product findProduct = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductException(ErrorCode.NOT_EXISTS_PRODUCT));
 
-        Product findProduct = productService.findById(productId);
+        if (findProduct.getProductBidPhase() == ProductBidPhase.ENDED) {
+            sendMessage(session, "경매가 종료되었습니다.");
+            return;
+        }
+
+        if (findProduct.getProductBidPhase() == ProductBidPhase.BEFORE) {
+            sendMessage(session, "경매가 시작되야 입찰을 할 수 있습니다..");
+            return;
+        }
+
         Set<WebSocketSession> bidRoomSessions = getWebSocketSessions(productId);
-
-        if (findProduct.getProductBidPhase() != ProductBidPhase.ONGOING) {
-            closeAllSessions(bidRoomSessions);
+        if (Objects.equals(findProduct.getMember().getId(), member.getId())) {
+            sendMessage(session, "상품 판매자는 입찰할 수 없습니다.");
+            return;
         }
 
         String payload = message.getPayload();
         MessageDto.Request dtoRequest = objectMapper.readValue(payload, MessageDto.Request.class);
 
         List<BidDto> bidList;
-        try {
-            bidList = bidListFromRedis(productId, roomSessionMap.get(productId));
-        } catch (BidException e) {
-            log.info("경매 종료={}", e.getMessage());
-            return;
-        }
+        bidList = bidListFromRedis(productId);
 
         Long currMaxBidAmount = getMaxBidAmount(bidList, findProduct.getStartBid());
         if (currMaxBidAmount >= dtoRequest.getBidAmount()) {
@@ -101,12 +109,30 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
             return;
         }
 
-        // 현재 잔액에서의 예외와 잔액 소모시켜야됨
-        // 잔액 돌려주기는 redis에서 해야할 듯??
-        // 가입안한 회원이와도 되는지 확인
+        if (dtoRequest.getMaxBidLimit() < dtoRequest.getBidAmount()) {
+            sendMessage(session, "입찰한계금액보다 입찰금액은 작아야합니다.");
+            return;
+        }
+
+        // 세션을 기준으로 돈 작성 시에 새로운 세션에의 같은 멤버는 제대로 된 입찰이 불가능 해짐
+        // 추후에 레디스를 이용해 별도로 입찰 금액을 꺼내와야 됨
+
+        try {
+            Long lastMoney = (Long) attributes.get("lastMoney");
+            if (lastMoney == null) {
+                lastMoney = 0L;
+            }
+
+            memberService.bidToAuction(member, dtoRequest.getBidAmount(), lastMoney);
+            attributes.put("lastMoney", dtoRequest.getBidAmount());
+        } catch (MoneyException | NullPointerException e) {
+            log.info("MoneyEx={}", e.getMessage());
+            sendMessage(session, "잔액이 부족합니다.");
+            return;
+        }
 
         MessageDto.Response response = MessageDto.Response
-                        .fromRequest(member, productId, dtoRequest.getBidAmount());
+                .fromRequest(member, productId, dtoRequest.getBidAmount());
 
         putInRedis(productId, member, dtoRequest, bidList);
         sendMessageToBidRoom(response, bidRoomSessions);
@@ -115,13 +141,11 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         Map<String, Object> attributes = session.getAttributes();
-        Member member;
-        Long productId;
-        try {
-            member = (Member) attributes.get(MEMBER);
-            productId = (Long) attributes.get(PRODUCT_ID);
-        } catch (NullPointerException e) {
-            log.info("NullPointerEx={}", e.getMessage());
+        Member member = (Member) attributes.get(MEMBER);
+        Long productId = (Long) attributes.get(PRODUCT_ID);
+
+        if (member == null) {
+            log.info("{} 연결 끊김", session.getId());
             return;
         }
 
@@ -129,6 +153,15 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
         sessions.remove(session);
         sendViewerCountToBidRoom(getWebSocketSessions(productId));
         log.info("{} 연결 끊김", member.getId());
+    }
+
+    public void phaseChange(Long productId, ProductBidPhase phase) {
+        Set<WebSocketSession> webSocketSessions = getWebSocketSessions(productId);
+        webSocketSessions.parallelStream().forEach(session -> sendMessage(session, phase));
+        if (phase == ProductBidPhase.ENDED){
+            closeAllSessions(webSocketSessions);
+        }
+
     }
 
     private void sendMessageToBidRoom(MessageDto.Response res, Set<WebSocketSession> bidRoomSession) {
@@ -143,6 +176,7 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
                 .filter(WebSocketSession::isOpen)
                 .forEach(sess -> sendMessage(sess, viewerCnt));
     }
+
 
     private <T> void sendMessage(WebSocketSession session, T message) {
         try {
@@ -161,7 +195,7 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
     }
 
     private Set<WebSocketSession> getWebSocketSessions(Long productId) {
-        if(!roomSessionMap.containsKey(productId)){
+        if (!roomSessionMap.containsKey(productId)) {
             roomSessionMap.put(productId, new HashSet<>());
         }
 
@@ -174,7 +208,7 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
     }
 
     private Set<WebSocketSession> getSocketSetWhenEntered(Long productId, WebSocketSession webSocketSession) {
-        if(!roomSessionMap.containsKey(productId)){
+        if (!roomSessionMap.containsKey(productId)) {
             roomSessionMap.put(productId, new HashSet<>());
         }
 
@@ -191,30 +225,28 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
         bidRoomSession.removeIf(sess -> !sessions.contains(sess));
     }
 
-    private List<BidDto> bidListFromRedis(Long productId, Set<WebSocketSession> bidRoomSession) {
+    private List<BidDto> bidListFromRedis(Long productId) {
         HashOperations<String, Long, List<BidDto>> auctionRedis = redisTemplate.opsForHash();
-
-        if (!auctionRedis.hasKey(AUCTION, productId)) {
-            closeAllSessions(bidRoomSession);
-            throw new BidException(ErrorCode.NOT_EXIST_AUCTION);
-        }
+        if (!auctionRedis.hasKey(AUCTION, productId)) return new ArrayList<>();
 
         return auctionRedis.get(AUCTION, productId);
     }
 
     private void closeAllSessions(Set<WebSocketSession> bidRoomSession) {
-        bidRoomSession.parallelStream().forEach(session -> {
-            Map<String, Object> attributes = session.getAttributes();
-            Long productId = (Long) attributes.get(PRODUCT_ID);
-            roomSessionMap.get(productId).remove(session);
-            try {
-                sendMessage(session, "경매를 진행할 수 없습니다.");
+        Iterator<WebSocketSession> iterator = bidRoomSession.iterator();
+
+        while (iterator.hasNext()) {
+            WebSocketSession session = iterator.next();
+            if (session.isOpen()) {
+                iterator.remove();
                 sessions.remove(session);
-                session.close();
-            } catch (IOException e) {
-                log.error("웹소켓 세션 종료 실패: sessionId={}", session.getId(), e);
+                try {
+                    session.close();
+                } catch (IOException e) {
+                    log.error("웹소켓 세션 종료 실패: sessionId={}", session.getId(), e);
+                }
             }
-        });
+        }
     }
 
     private void closeOneSession(WebSocketSession session) {
@@ -244,7 +276,7 @@ public class WebSocketBidHandler extends TextWebSocketHandler {
     }
 
     private Long getMaxBidAmount(List<BidDto> bidList, long startBid) {
-        if (bidList.isEmpty()){
+        if (bidList.isEmpty()) {
             return startBid - 1;
         }
 
