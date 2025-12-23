f919739143d8a88e1f89214dd28802b82a797d37
JUNYOUNG\compu <computer9302@gmail.com>
2025-12-23 16:22:51 +0900

    REFACTOR: DB정규화코드수정 - Bid 클래스 수정

--- src/main/java/com/auction/bid/domain/bid/Bid.java
+++ src/main/java/com/auction/bid/domain/bid/Bid.java
@@ -1,10 +1,12 @@
 package com.auction.bid.domain.bid;
 
+import com.auction.bid.domain.auction.Auction;
 import com.auction.bid.domain.member.Member;
 import com.auction.bid.domain.product.Product;
 import com.auction.bid.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
@@ -24,8 +26,8 @@ public class Bid extends BaseEntity {
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    private Product product;
+    @JoinColumn(name = "auction_id")
+    private Auction auction;
 
     private Long bidAmount;
     private LocalDateTime bidTime;
